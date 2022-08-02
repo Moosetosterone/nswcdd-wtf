55fcfc7b402703c54bad5c2cd8805b002097c225
Nick Moose <nicholas.w.moose.civ@us.navy.mil>
2022-08-02 11:48:49 -0400

    deleted all code, made the file blank

--- resources/js/main.js
+++
@@ -1,4 +0,0 @@
-var lonlat = [-76.4377, 38.2797]; //[-67.212, 37.114]; //[-75.23825026538958, 35.71937869595126];
-import { drawMap, drawMapLibre } from "./resources/js/mapping.js";
-//var my_map = drawMap({ lonlat: lonlat, startZoom: 5 });
-drawMapLibre({ lonlat: lonlat, startZoom: 5 });
